fecc4c06a0c5ddf2a68c44c5a3f52fb2050e52f3
David Cornejo <davidcornejo1@live.com>
2025-07-08 20:12:25 -0600

    Format module native code

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,6 @@
 module.exports = {
   "**/*.js?(x)": "eslint --fix",
   "**/*.ts?(x)": [() => "tsc --noEmit --skipLibCheck", "eslint --fix"],
+  "**/*.kt": "ktlint android/ --format",
+  "**/*.swift": "swiftformat ios/ --swiftversion 5.4",
 };
