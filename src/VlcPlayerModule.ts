3abecf205db5ec84c7840af9b5e150c5bced5a23
David Cornejo <davidcornejo1@live.com>
2025-07-10 08:29:20 -0600

    Remove unnecessary whitespace

--- src/VlcPlayerModule.ts
+++ src/VlcPlayerModule.ts
@@ -3,5 +3,5 @@ import { NativeModule, requireNativeModule } from "expo";
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 declare class VlcPlayerModule extends NativeModule<{}> {}
 
-// This call loads the native module object from the JSI.
+// This call loads the native module object from the JSI
 export default requireNativeModule<VlcPlayerModule>("ExpoLibVlcPlayer");
