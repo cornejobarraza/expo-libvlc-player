cb631705ce22ea2cb74a3a4e779e32772c3ba70d
David Cornejo <davidcornejo1@live.com>
2025-07-10 08:36:34 -0600

    Update ESLint config file

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 const { fixupConfigRules } = require("@eslint/compat");
 const { FlatCompat } = require("@eslint/eslintrc");
 const js = require("@eslint/js");
-const { defineConfig, globalIgnores } = require("eslint/config");
+const { defineConfig } = require("eslint/config");
 const globals = require("globals");
 
 const compat = new FlatCompat({
@@ -17,10 +17,15 @@ module.exports = defineConfig([
       "no-restricted-imports": [
         "warn",
         {
+          // fbjs is a Facebook-internal package not intended to be a public API
           patterns: ["fbjs/*", "fbjs"],
         },
       ],
     },
+    ignores: ["**/build"],
+  },
+  {
+    files: ["**/__tests__/*"],
     languageOptions: {
       globals: {
         ...globals.node,
@@ -29,22 +34,21 @@ module.exports = defineConfig([
     },
   },
   {
-    files: ["**/__tests__/*"],
-  },
-  {
-    files: ["./*.config.js", "./.*rc.js"],
     extends: [...fixupConfigRules(compat.extends("universe/node"))],
+    files: ["./*.config.js", "./.*rc.js"],
   },
   {
     files: ["**/*.ts", "**/*.tsx", "**/*.d.ts"],
     rules: {
+      // NOTE: This is already handled by TypeScript itself
+      // Turning this on blocks legitimate type overloads
       "no-redeclare": "off",
       "@typescript-eslint/no-redeclare": "off",
+      // NOTE: Handled by TypeScript
       "no-unused-expressions": "off",
       "no-unused-vars": "off",
       "@typescript-eslint/no-unused-expressions": "off",
       "@typescript-eslint/no-unused-vars": "off",
     },
   },
-  globalIgnores(["**/build"]),
 ]);
