30487bc45de286dcd30ae72fc85893735bae4573
David Cornejo <davidcornejo1@live.com>
2026-02-20 13:49:14 -0600

    Remove plugin build folder

--- plugin/build/withExpoLibVlcPlayer.d.ts
+++
@@ -1,7 +0,0 @@
-import { type ConfigPlugin } from "expo/config-plugins";
-type WithExpoLibVlcPlayerOptions = {
-    localNetworkPermission?: string | false;
-    supportsBackgroundPlayback?: boolean;
-};
-declare const withExpoLibVlcPlayer: ConfigPlugin<WithExpoLibVlcPlayerOptions>;
-export default withExpoLibVlcPlayer;
