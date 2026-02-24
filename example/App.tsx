f7b77164f9465b4cc88153b55d2d1b9815e1ae15
David Cornejo <davidcornejo1@live.com>
2026-02-23 19:21:54 -0600

    Update example app

--- example/App.tsx
+++ example/App.tsx
@@ -15,24 +15,22 @@ import { IconSymbol } from "./components/IconSymbol";
 const BIG_BUCK_BUNNY =
   "https://download.blender.org/peach/bigbuckbunny_movies/big_buck_bunny_720p_h264.mov";
 
+const MIN_VOLUME = 0;
+const VOLUME_STEP = 10;
+const MAX_VOLUME = 100;
+
+const DEFAULT_TIME = 0;
+const BUFFERING_DELAY = 1_000;
+const SEEK_STEP = 5_000;
+
 export default function App() {
   const [buffering, setBuffering] = useState<boolean>(false);
   const [playing, setPlaying] = useState<boolean>(true);
-  const [time, setTime] = useState<number>(0);
-  const [volume, setVolume] = useState<number>(100);
+  const [time, setTime] = useState<number>(DEFAULT_TIME);
+  const [volume, setVolume] = useState<number>(MAX_VOLUME);
 
-  const playerRef = useRef<LibVlcPlayerViewRef | null>(null);
-  const bufferingRef = useRef<NodeJS.Timeout | null>(null);
-
-  const handleBuffering = () => {
-    setBuffering(true);
-
-    if (bufferingRef.current) {
-      clearTimeout(bufferingRef.current);
-    }
-
-    bufferingRef.current = setTimeout(() => setBuffering(false), 1_000);
-  };
+  const playerRef = useRef<LibVlcPlayerViewRef>(null);
+  const bufferingRef = useRef<NodeJS.Timeout>(undefined);
 
   return (
     <View style={styles.app}>
@@ -51,7 +49,14 @@ export default function App() {
           style={styles.player}
           source={BIG_BUCK_BUNNY}
           volume={volume}
-          onBuffering={handleBuffering}
+          onBuffering={() => {
+            setBuffering(true);
+            clearTimeout(bufferingRef.current);
+            bufferingRef.current = setTimeout(
+              () => setBuffering(false),
+              BUFFERING_DELAY,
+            );
+          }}
           onPlaying={() => setPlaying(true)}
           onPaused={() => setPlaying(false)}
           onStopped={() => setPlaying(false)}
@@ -59,22 +64,23 @@ export default function App() {
           onTimeChanged={({ time }) => setTime(time)}
         />
       </View>
-      <View style={styles.buttons}>
+      <View style={styles.controls}>
         <TouchableOpacity
-          style={styles.button}
-          onPress={() => playerRef.current?.seek(time - 5_000)}
+          style={styles.control}
+          onPress={() => playerRef.current?.seek(time - SEEK_STEP)}
         >
           <IconSymbol color="#f1f1f1" name="backward.fill" />
         </TouchableOpacity>
         <TouchableOpacity
-          style={styles.button}
-          onPress={() => setVolume((prev) => prev - 10)}
-          disabled={volume === 0}
+          style={styles.control}
+          onPress={() =>
+            setVolume((prev) => Math.max(prev - VOLUME_STEP, MIN_VOLUME))
+          }
         >
           <IconSymbol color="#f1f1f1" name="speaker.1.fill" />
         </TouchableOpacity>
         <TouchableOpacity
-          style={styles.button}
+          style={styles.control}
           onPress={() => playerRef.current?.[!playing ? "play" : "pause"]()}
         >
           <IconSymbol
@@ -83,21 +89,22 @@ export default function App() {
           />
         </TouchableOpacity>
         <TouchableOpacity
-          style={styles.button}
+          style={styles.control}
           onPress={() => playerRef.current?.stop()}
         >
           <IconSymbol color="#f1f1f1" name="stop.fill" />
         </TouchableOpacity>
         <TouchableOpacity
-          style={styles.button}
-          onPress={() => setVolume((prev) => prev + 10)}
-          disabled={volume === 100}
+          style={styles.control}
+          onPress={() =>
+            setVolume((prev) => Math.min(prev + VOLUME_STEP, MAX_VOLUME))
+          }
         >
           <IconSymbol color="#f1f1f1" name="speaker.3.fill" />
         </TouchableOpacity>
         <TouchableOpacity
-          style={styles.button}
-          onPress={() => playerRef.current?.seek(time + 5_000)}
+          style={styles.control}
+          onPress={() => playerRef.current?.seek(time + SEEK_STEP)}
         >
           <IconSymbol color="#f1f1f1" name="forward.fill" />
         </TouchableOpacity>
@@ -130,12 +137,12 @@ const styles = StyleSheet.create({
     backgroundColor: "black",
     borderRadius: 12,
   },
-  buttons: {
+  controls: {
     flexDirection: "row",
     justifyContent: "center",
     gap: 16,
   },
-  button: {
+  control: {
     backgroundColor: "#272727",
     paddingVertical: 4,
     paddingHorizontal: 8,
