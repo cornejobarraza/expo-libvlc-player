b02703c2ea4a6f4988ba5e384910819c35bc606a
David Cornejo <davidcornejo1@live.com>
2026-04-22 11:03:27 -0600

    Add .swift-version file

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,5 +2,5 @@ module.exports = {
   "**/*.js?(x)": "eslint --fix",
   "**/*.ts?(x)": [() => "tsc --noEmit", "eslint --fix"],
   "**/*.kt": "ktlint android/ --format",
-  "**/*.swift": "swiftformat ios/ --swiftversion 5.9",
+  "**/*.swift": "swiftformat ios/",
 };
