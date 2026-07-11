5b8abefe5e2de3b210d116ea378c558e89448510
David Cornejo <davidcornejo1@live.com>
2026-07-11 14:19:50 -0600

    Fix double quotes regression

--- android/src/main/java/expo/modules/libvlcplayer/LibVlcPlayerView.kt
+++ android/src/main/java/expo/modules/libvlcplayer/LibVlcPlayerView.kt
@@ -867,7 +867,7 @@ private fun MutableList<String>.normalizeOptions() {
     val normalized =
         map { option ->
             if (!option.startsWith(":")) {
-                ":" + option.dropWhile { character -> character == "-" }
+                ":" + option.dropWhile { character -> character == '-' } // Single quotes required
             } else {
                 option
             }
