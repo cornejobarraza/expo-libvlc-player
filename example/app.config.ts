dce5f915208eccef7f4e6bf4779a94fdc2a67b80
David Cornejo <davidcornejo1@live.com>
2026-09-09 09:47:02 -0600

    Use TypeScript for configuration

---
+++ example/app.config.ts
@@ -0,0 +1,24 @@
+import { type ExpoConfig, type ConfigContext } from "expo/config";
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: "LibVLC Player",
+  slug: "expo-libvlc-player-example",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  android: {
+    package: "expo.modules.libvlcplayer.example",
+    adaptiveIcon: {
+      backgroundColor: "#e6f4fe",
+      foregroundImage: "./assets/android-icon-foreground.png",
+      backgroundImage: "./assets/android-icon-background.png",
+      monochromeImage: "./assets/android-icon-monochrome.png",
+    },
+  },
+  ios: {
+    bundleIdentifier: "expo.modules.libvlcplayer.example",
+    supportsTablet: true,
+  },
+  plugins: ["../app.plugin.js", "expo-font"],
+});
